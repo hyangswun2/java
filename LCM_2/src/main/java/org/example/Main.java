cc870a3344ba04bb68cfb572859d9d21389e05b3
kimdaeyoung <crossapp86@gmail.com>
2023-07-03 18:15:49 +0900

    second commit

--- LCM_2/src/main/java/org/example/Main.java
+++ LCM_2/src/main/java/org/example/Main.java
@@ -4,13 +4,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(LCM(1, 20));
     }
-
     private static long LCM(int start, int end) {
         long lcm = start;
         for (int i = start + 1; i <= end; i++) lcm = getLCM(lcm, i);
         return lcm;
     }
-
     private static long getLCM(long a, long b) {
         return (a * b) / getGCD(a, b);
     }
